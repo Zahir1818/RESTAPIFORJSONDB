2f0c38fbf78d5ef42ff897f8ea1e808c972cb655
zahir <zahir@gamil.com>
2023-09-30 14:54:49 +0600

    Upload rest api project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+
+
+//environment variable
+const PORT = process.env.PORT || 8080;
+
+
+
+//express init
+const app = express();
+
+
+//express middlewares
+app.use(express.json());
+app.use(express.urlencoded( { extended : false } ));
+
+
+
+//listen port
+app.listen(PORT, () => {
+    console.log(`server is running on port ${ PORT }`.bgGreen.black);
+});
